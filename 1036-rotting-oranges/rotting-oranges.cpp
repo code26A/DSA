84c0ac376605f555b7c66cacfd3cfee680c2bc4f
Siddharth Shankar <104447344+code26A@users.noreply.github.com>
2026-03-22 20:05:28 +0530

    Time: 3 ms (27.61%) | Memory: 18 MB (6.08%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    bool safe(int i,int j,vector<vector<int>> &grid){
-        return i>=0 && j>=0 && i<grid.size() && j<grid[0].size();
-    }
     int orangesRotting(vector<vector<int>>& grid) {
-         queue<pair<int,int>> q;
-         int fresh=0;
-         for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==2){
-                     q.push({i,j});
-                }else if(grid[i][j]==1) fresh++;
+        int n=grid.size();
+        int m=grid[0].size();
+        int fresh=0;
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2) q.push({i,j});
+                if(grid[i][j]==1) fresh++;
             }
-         }
-         int time=0;
-         vector<vector<int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
-         while(!q.empty()){
-            int n=q.size();
-            bool spread=false;
-            for(int i=0;i<n;i++){
+        }
+        int time=0;
+        vector<vector<int>> dir={{1,0},{0,1},{-1,0},{0,-1}};
+        while(!q.empty()){
+            int lev=q.size();
+            bool flag=false;
+            for(int i=0;i<lev;i++){
                 auto [row,col]=q.front();
-                q.pop();    
-                for(auto &it:dir){
-                    int nrow=row+it[0];
-                    int ncol=col+it[1];
-                    if(safe(nrow,ncol,grid) && grid[nrow][ncol]==1){
-                        grid[nrow][ncol]=2;
+                q.pop();
+                for(auto it: dir){
+                    int nx=row+it[0];
+                    int ny=col+it[1];
+                    if(nx<n && ny<m && nx>=0 && ny>=0 && grid[nx][ny]==1){
+                        grid[nx][ny]=2;
+                        q.push({nx,ny});
+                        flag=true;
                         fresh--;
-                        spread=true;
-                        q.push({nrow,ncol});
                     }
                 }
             }
-            if(spread) time++;
-         }
-         return fresh==0?time:-1;
+            if(flag) time++;
+        }
+        if(fresh!=0) return -1;
+        return time;
     }
 };
